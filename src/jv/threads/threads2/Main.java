ef7779292f3a7aa80fd8347918d2349cbffe6937
gilbertosantana <gilbertosantoss307@gmail.com>
2026-09-10 16:16:36 -0300

    Threads: Interface Runnable

---
+++ src/jv/threads/threads2/Main.java
@@ -0,0 +1,12 @@
+package jv.threads.threads2;
+
+public class Main {
+    public static void main(String[] args) {
+        MinhaThreadRunnable thread1 = new  MinhaThreadRunnable("#1", 900);
+        /*Thread t1 = new Thread(thread1);
+        t1.start();*/
+
+        MinhaThreadRunnable thread2 = new  MinhaThreadRunnable("#2", 650);
+        MinhaThreadRunnable thread3 = new  MinhaThreadRunnable("#3", 1100);
+    }
+}
